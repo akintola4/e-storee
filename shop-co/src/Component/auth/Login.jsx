81dc0787e08198566d2fc25eeb59675e1b194bb3
akintola4 <akintope830@gmail.com>
2024-02-04 01:30:08 +0100

    removed the alert

--- shop-co/src/Component/auth/Login.jsx
+++ shop-co/src/Component/auth/Login.jsx
@@ -1,12 +1,14 @@
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { Link, Navigate } from "react-router-dom";
+import { IoClose } from "react-icons/io5";
+
 import Profile from "./Profile";
 import { useState } from "react";
 import { supabase } from '../supabase-context/client'
 import { useNavigate } from "react-router-dom";
 export default function Login({ setToken }) {
-
+    const [showModal, setShowModal] = useState(true);
     const navigate = useNavigate()
     const [formData, setFormData] = useState(
         {
@@ -34,7 +36,7 @@ export default function Login({ setToken }) {
                 navigate('/Profile')
             }
             else if (data.user == null) {
-                alert("Invalid Credentials")
+                setShowModal(true)
             }
         } catch (error) {
             alert(error)
@@ -48,6 +50,52 @@ export default function Login({ setToken }) {
     }
     return (
         <div>
+              {showModal ?
+          <div >
+            <div>
+          <>
+                                <div className="fixed inset-0 z-50 flex justify-end m-5 overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+                                    <div className="relative my-6">
+                                        <div className="relative flex flex-col w-full p-5 bg-white border-0 shadow-lg outline-none rounded-xl focus:outline-none">
+                                            <div
+                                                class="flex flex-shrink-0 items-center justify-between rounded-t-md bg-info-600 gap-20 p-4 dark:border-b dark:border-neutral-500 dark:bg-transparent">
+                                                <h5
+                                                    class="text-sm md:text-xl font-medium leading-normal "
+                                                    id="rightTopModalLabel">
+                                                   Login status
+                                                </h5>
+                                                <button
+                                                    className="text-2xl text-zinic-600"
+                                                    onClick={() => setShowModal(false)}
+                                                >
+                                                    <IoClose />
+                                                </button>
+                                            </div>
+                                            <div className='flex flex-col items-center gap-2 p-5 text-center'>
+                                                <IoClose className="p-3 text-5xl border-2 border-black rounded-full text-zinic-600" />
+
+                                                <h4 className='text-lg font-bold md:text-2xl'>Not logged in</h4>
+                                                <p className='text-sm md:text-md text-zinc-700'>Invalid Credentials</p>
+                                            </div>
+                                            <Link
+                                                className="p-2 text-center text-white bg-black border-2 border-black rounded-full cursor-pointer hover:border-black hover:border-2 hover:bg-white hover:text-black hover:bg-zinic-600"
+                                                type="button"
+                                                to={"/Login"}
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Try again
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>: <></>
+            </div>
+           
+
+          </div>
+
+          : null}
+        {/* react routes will contain all the routes we are going to use */}
             <main className="flex flex-col gap-5 px-5 mx-auto my-32 xl:px-60 md:pt-20 accent-green-950 md:my-14">
                 <section className="flex items-center justify-center gap-10 rounded-lg md:justify-normal ">
                     <div className="hidden w-6/12 title md:block ">
